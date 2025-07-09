2bee6b167cd3fa1000ee6649b650463356d947bc
HeliosZhao <yuyangzhao@outlook.com>
2025-07-09 03:37:54 +0000

    update check data

--- check_data.py
+++ check_data.py
@@ -9,34 +9,41 @@ args = parser.parse_args()
 
 data_path = args.data_path
 zip_fns = sorted([f for f in os.listdir(data_path) if f.endswith(".zip")])
+if not len(zip_fns):
+    zip_fns = sorted([f for f in os.listdir(data_path) if f.endswith("_dover.json")])
+
+num_files = 0
 for zip_fn in tqdm(zip_fns):
     fn = zip_fn.split(".")[0]
+    if fn.endswith("_dover"):
+        fn = fn[:-6]
     vmaf = os.path.join(data_path, f"{fn}_vmafmotion.json")
     unimatch = os.path.join(data_path, f"{fn}_unimatch.json")
     dover = os.path.join(data_path, f"{fn}_dover.json")
     # check if all exist, and if not, print the missing one
+    existing = {}
     missing = []
-    if not os.path.exists(vmaf):
-        missing.append(vmaf)
-    if not os.path.exists(unimatch):
-        missing.append(unimatch)
-    if not os.path.exists(dover):
-        missing.append(dover)
-    if len(missing) > 0:
-        print(f"missing files: {missing}")
-        continue
+    if os.path.exists(vmaf):
+        existing["vmaf"] = json.load(open(vmaf, "r"))
+    if os.path.exists(unimatch):
+        existing["unimatch"] = json.load(open(unimatch, "r"))
+    if os.path.exists(dover):
+        existing["dover"] = json.load(open(dover, "r"))
+    if len(existing) != 3:
+        missing = set(['vmaf', 'unimatch', 'dover']) - set(existing.keys())
+        if 'dover' in missing:
+            break
+        print(f"missing files: {fn}, {missing}")
+        
+        for k in missing:
+            existing[k] = {}
+
+    print(f"{fn}, {len(existing['vmaf'])}, {len(existing['unimatch'])}, {len(existing['dover'])}")
     
-    # check if the length of the three files are the same
-    with open(vmaf, "r") as f:
-        vmaf_data = json.load(f)
-    with open(unimatch, "r") as f:
-        unimatch_data = json.load(f)
-    with open(dover, "r") as f:
-        dover_data = json.load(f)
-    if len(vmaf_data) != len(unimatch_data) or len(vmaf_data) != len(dover_data):
-        print(f"lengths are not the same {fn}: vmaf {len(vmaf_data)}, unimatch {len(unimatch_data)}, dover {len(dover_data)}")
-        continue
     # check if the length is too short
-    if len(vmaf_data) < 9900:
-        print(f"Data length is less than 9900: {fn}, length: {len(vmaf_data)}")
-        continue
+    if len(existing['dover']) < 9900:
+        print(f"Data length is less than 9900: {fn}, length: {len(existing['dover'])}")
+        # continue
+    num_files += len(existing['dover'])
+
+print(f"num_files: {num_files}")
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,15 +3,15 @@
 #SBATCH -p polar4,polar3,polar,grizzly                                         #partition
 #SBATCH -t 04:00:00                                                                 #wall time limit, hr:min:sec
 #SBATCH -N 1                                                                        #number of nodes
-#SBATCH -J elm:sora_new_0704                                                       #job name
-#SBATCH --array=0-432%433                                                          # 创建一个作业数组，包含从0到511的作业实例，每次最多并行运行40个实例
+#SBATCH -J elm:mjv                                                       #job name
+#SBATCH --array=0-302%303                                                          # 创建一个作业数组，包含从0到511的作业实例，每次最多并行运行40个实例
 #SBATCH --gpus-per-node 1
 #SBATCH --cpus-per-task 12
 #SBATCH --mem-per-cpu 16G
 #SBATCH -e /home/lezhao/lezhao/DOVER/output/slurm_log_dover/dev_error.log
 #SBATCH -o /home/lezhao/lezhao/DOVER/output/slurm_log_dover/dev_out.log
 
-job_shift=2651
+job_shift=0
 idx=$(($SLURM_ARRAY_TASK_ID + job_shift))
 total=$(($SLURM_ARRAY_TASK_COUNT + job_shift))
 
@@ -21,11 +21,11 @@ export PATH="$HOME/miniforge3/envs/dover/bin:$PATH"
 # DATA_PATH="data/InternData/debug_data_train/wan/"       # 69 zips
 # DATA_PATH="data/InternData/debug_data_train/wan_slg/"   # 553 zips
 # DATA_PATH="data/sora"        # 2651 zips
-# DATA_PATH="data/mjv" # 0 - 290 291 zips
-DATA_PATH="data/sora_new_0704" # 2651-3084 433 zips
+DATA_PATH="data/mjv" # 0 - 302 303 zips
+# DATA_PATH="data/sora_new_0704" # 2651-3084 433 zips
 # SAVE_DIR="output/sora"
-SAVE_DIR="output/sora_new_0704"
-# SAVE_DIR="output/mjv"
+# SAVE_DIR="output/sora_new_0704"
+SAVE_DIR="output/mjv"
 mkdir -p $SAVE_DIR
 
 logdir=$SAVE_DIR/slurm_logs
